834f23f4e78a7ed9417d348e33d1ec673e612626
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-06-02 06:43:18 -0700

    js imtihon

--- main.js
+++ main.js
@@ -1,64 +1,23 @@
-let a ="bir"
-let b = "ikki"
-let c = "bush"
-a=c
-b=a
-c=a
-
-
-console.log(a)
-console.log(b)
-
-
-
-
-
-
-
-
-// let baho = prompt("baho quying")*1
-// if (baho >= 90 &&  baho <= 100) {
-//     console.log("A")
-// }else if (baho >= 80 &&  baho <= 89) {
-//     console.log("B")
-// }else if (baho >= 70 &&  baho <= 79) {
-//     console.log("C")
-// }else if (baho >= 60 &&  baho <=69) {
-//     console.log("D")
-// }else if (baho >= 0 &&  baho <= 59) {
-//     console.log("F")
+// let a = prompt("son kiriting")
+// if (a*1 % 2 === 0) {
+//     console.log("bu juft son")
 // }else {
-//     console.log("Bunaqa baho quyib bulmaydi")
+//     console.log("bu toq son")
 // }
 
+// let x = [1, 2, 3, 4, 5, 6, 7, 8]
+//  x.filter(x => 0)
+// console.log(x + " " + "Jami: " + x.length)
 
-
-
-
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//     }
+// let x = prompt('Ism kiriting!');
+// function name (data){
+//     alert(data.split("").reverse().join(""))
 // }
+// name(x)
 
 
 
 
-
-// let a = prompt ("son kiriting")
-// console.log(a*a)
-
-
-
-
-
-
-// let user = {
-//     name:"ahmad",
-//     age:"16",
-//     surname:"ibrohimov",
-// }
-// console.log(user);
 
 
 
